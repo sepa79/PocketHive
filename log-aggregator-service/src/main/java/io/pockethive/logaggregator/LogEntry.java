d361417960cf5a5c881680b917b29d49656be085
sepa79 <37452386+sepa79@users.noreply.github.com>
2025-09-02 14:13:15 +0100

    fix: handle unknown log fields

--- log-aggregator-service/src/main/java/io/pockethive/logaggregator/LogEntry.java
+++ log-aggregator-service/src/main/java/io/pockethive/logaggregator/LogEntry.java
@@ -1,3 +1,13 @@
 package io.pockethive.logaggregator;
 
-public record LogEntry(String service, String traceId, String level, String message, String timestamp) {}
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record LogEntry(
+    String service,
+    String traceId,
+    String level,
+    String message,
+    @JsonProperty("@timestamp") String timestamp
+) {}
