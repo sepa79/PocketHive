79f4d35d9c3a7a5f8409df292757765b72410b71
sepa79 <37452386+sepa79@users.noreply.github.com>
2025-09-04 22:54:51 +0100

    fix(orchestrator): simplify docker client configuration

--- orchestrator-service/src/main/java/io/pockethive/orchestrator/infra/docker/DockerConfiguration.java
+++ orchestrator-service/src/main/java/io/pockethive/orchestrator/infra/docker/DockerConfiguration.java
@@ -3,8 +3,6 @@ package io.pockethive.orchestrator.infra.docker;
 import com.github.dockerjava.api.DockerClient;
 import com.github.dockerjava.core.DefaultDockerClientConfig;
 import com.github.dockerjava.core.DockerClientImpl;
-import com.github.dockerjava.transport.DockerHttpClient;
-import com.github.dockerjava.transport.httpclient5.ApacheDockerHttpClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,11 +11,7 @@ public class DockerConfiguration {
     @Bean
     public DockerClient dockerClient() {
         DefaultDockerClientConfig config = DefaultDockerClientConfig.createDefaultConfigBuilder().build();
-        DockerHttpClient httpClient = new ApacheDockerHttpClient.Builder()
-                .dockerHost(config.getDockerHost())
-                .sslConfig(config.getSSLConfig())
-                .build();
-        return DockerClientImpl.getInstance(config, httpClient);
+        return DockerClientImpl.getInstance(config);
     }
 
     @Bean
