66adc46dbd922bea5f3559d086f4e49c936463f0
sepa79 <37452386+sepa79@users.noreply.github.com>
2025-10-08 14:45:17 +0100

    Enable Rabbit infrastructure for processor example

--- examples/worker-starter/processor-worker/src/main/java/io/pockethive/examples/starter/processor/ProcessorWorkerApplication.java
+++ examples/worker-starter/processor-worker/src/main/java/io/pockethive/examples/starter/processor/ProcessorWorkerApplication.java
@@ -2,10 +2,12 @@ package io.pockethive.examples.starter.processor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.rabbit.annotation.EnableRabbit;
 
 /**
  * Minimal Spring Boot entrypoint showing how to bootstrap a processor worker service.
  */
+@EnableRabbit
 @SpringBootApplication
 public class ProcessorWorkerApplication {
 
