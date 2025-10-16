e54005c4864f1f622cf47a8c1e969c0d493499eb
sepa79 <37452386+sepa79@users.noreply.github.com>
2025-10-16 17:38:59 +0100

    Remove redundant bee identity logging

--- trigger-service/src/main/java/io/pockethive/trigger/Application.java
+++ trigger-service/src/main/java/io/pockethive/trigger/Application.java
@@ -1,26 +1,14 @@
 package io.pockethive.trigger;
 
-import io.pockethive.controlplane.spring.BeeIdentityProperties;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @EnableRabbit
 @SpringBootApplication
 public class Application {
 
-  private static final Logger log = LoggerFactory.getLogger(Application.class);
-
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
-  }
-
-  @Bean
-  ApplicationRunner beeIdentityLogger(BeeIdentityProperties beeIdentityProperties) {
-    return args -> log.info("Bee name: {}", beeIdentityProperties.beeName());
   }
 }
