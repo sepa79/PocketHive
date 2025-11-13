61c460700ceb2d063ab6d1404ee36c9a9c2646b3
Sepa <sepa79@gmail.com>
2025-11-13 00:27:16 +0000

    wip

--- payload-generator-service/src/main/java/io/pockethive/payload/generator/PayloadGeneratorApplication.java
+++ payload-generator-service/src/main/java/io/pockethive/payload/generator/PayloadGeneratorApplication.java
@@ -1,12 +1,8 @@
 package io.pockethive.payload.generator;
 
-import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableRabbit
-@EnableScheduling
 @SpringBootApplication
 public class PayloadGeneratorApplication {
 
