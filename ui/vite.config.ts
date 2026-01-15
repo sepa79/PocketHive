fa2a822e32249abd7eac3adb9b25978407eecae4
Sepa <sepa79@gmail.com>
2026-01-15 18:06:45 +0000

    fix: build UI for GitHub Pages

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.VITE_BASE ?? '/',
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   plugins: [react() as any],
   publicDir: 'assets',
